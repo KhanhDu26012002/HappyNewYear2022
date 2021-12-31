ed6992ac45188a06e5773f0bbc27b969536be5e7
Trần Khánh Dư <93703715+KhanhDu26012002@users.noreply.github.com>
2021-12-31 13:00:58 +0700

    Update main.js

--- main.js
+++ main.js
@@ -34,13 +34,3 @@ document.addEventListener("click", function()
 {
     window.location="./index.html";
 });
-function {
-      var myAudio = document.getElementById('playAudio');
-      if (myAudio.duration > 0 && !myAudio.paused) {
-          //Its playing...do your job
-
-      } else {
-        myAudio.play()
-        //Not playing...maybe paused, stopped or never played.
-
-      }
